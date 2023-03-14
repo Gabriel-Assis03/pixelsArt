e04dbcb9e428a3c3c78d5dc5fcd5c70999ba71d0
Gabriel do Carmo Assis <gabrieleboltffggff@gmail.com>
2023-03-14 16:27:38 -0300

    R5

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
+//localStorage.clear()
 const color1 = document.querySelector("#color1");
 const color2 = document.querySelector("#color2");
 const color3 = document.querySelector("#color3");
 const color4 = document.querySelector("#color4");
 const btnColorRandom = document.querySelector("#button-random-color");
+if(JSON.parse(localStorage.getItem('colorPalette')) == null) {
+
+    var colorPalette = {
+        color2: "red",
+        color3: "green",
+        color4: "blue",
+    }
+
+}else {
+
+var colorPalette = JSON.parse(localStorage.getItem('colorPalette'));
+
+}
+
+color2.style.background = colorPalette.color2;
+color3.style.background = colorPalette.color3;
+color4.style.background = colorPalette.color4;
 
 function mathRandom(max, min) {
     return Math.floor(Math.random() * max+min);
 }
-
-//console.log(Math.floor(Math.random() * 255+1));
 
 btnColorRandom.addEventListener("click", () => {
 
@@ -17,5 +33,11 @@ btnColorRandom.addEventListener("click", () => {
     color3.style.background = `rgb(${mathRandom(255, 1)},${mathRandom(255, 1)},${mathRandom(255, 1)})`
 
     color4.style.background = `rgb(${mathRandom(255, 1)},${mathRandom(255, 1)},${mathRandom(255, 1)})`
+
+    colorPalette.color2 = color2.style.background;
+    colorPalette.color3 = color3.style.background;
+    colorPalette.color4 = color4.style.background;
+
+    localStorage.setItem('colorPalette', JSON.stringify(colorPalette));
 
 })
